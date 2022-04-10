63f5945f26dd1328f73817043dd23361e8b19c37
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-10 21:10:01 +0700

    better mappings

--- core/polish/mappings.lua
+++ core/polish/mappings.lua
@@ -1,28 +1,23 @@
-local opts = { noremap = true, silent = true }
-local map = vim.api.nvim_set_keymap
-local kmap = vim.keymap.set
+local map = vim.keymap.set
 local unmap = vim.keymap.del
 -- Set key bindings
-map("n", "<A-k>", "<cmd>m .-2<CR>", opts)
-map("n", "<A-j>", "<cmd>m .+1<CR>", opts)
-map("n", "n", "nzzzv", opts)
-map("n", "N", "Nzzzv", opts)
-map("n", "J", "mzJ`z", opts)
-map("i", ",", ",<c-g>u", opts)
-map("i", ".", ".<c-g>u", opts)
-map("i", "!", "!<c-g>u", opts)
-map("i", "?", "?<c-g>u", opts)
-map("v", "<A-j>", ":m '>+1<cr>gv=gv", opts)
-map("v", "<A-k>", ":m '<-2<cr>gv=gv", opts)
-map("n", "<C-w>x", ":WinShift swap<cr>", opts)
-kmap("n", "<C-s>", vim.diagnostic.open_float)
-kmap("i", "<C-s>", vim.diagnostic.open_float)
-
--- Hop
-map("n", "f", ":HopChar1CurrentLine<cr>", opts)
-map("n", "F", ":HopChar2<cr>", opts)
+map("n", "<A-k>", "<cmd>m .-2<CR>")
+map("n", "<A-j>", "<cmd>m .+1<CR>")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+map("n", "J", "mzJ`z")
+map("i", ",", ",<c-g>u")
+map("i", ".", ".<c-g>u")
+map("i", "!", "!<c-g>u")
+map("i", "?", "?<c-g>u")
+map("v", "<A-j>", ":m '>+1<cr>gv=gv")
+map("v", "<A-k>", ":m '<-2<cr>gv=gv")
+map("n", "<C-w>x", ":WinShift swap<cr>")
+map("n", "<C-s>", vim.diagnostic.open_float)
+map("i", "<C-s>", vim.diagnostic.open_float)
+map("n", "f", ":HopChar1CurrentLine<cr>")
+map("n", "F", ":HopChar2<cr>")
 -- Unmap
--- nunmap "<leader>fw"
 unmap("n", "<leader>fw")
 unmap("n", "<leader>fo")
 unmap("n", "<leader>ff")
