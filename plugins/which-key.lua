4a0ac6d30da000641509635f3a0d92582ef054d3
Pham Dat <phamthanhdat17092004@gmail.com>
2022-04-16 18:55:02 +0700

    reconfig whichkey and bufferline

--- plugins/which-key.lua
+++ plugins/which-key.lua
@@ -3,11 +3,17 @@ return {
 	register_n_leader = {
 		["."] = { "<cmd>NeoRoot<cr>", "Set Root" },
 		["q"] = { ":Bdelete!<cr>", "Quit Buffer" },
-		["j"] = { ":lua require('nvim-window').pick()<cr>", "Jump Win" },
-		["u"] = { ":MundoToggle<cr>", "Undo List" },
+		["j"] = {
+			function()
+				require("nvim-window").pick()
+			end,
+			"Jump Win",
+		},
 		["w"] = { ":WinShift<cr>", "WinShift" },
 		["o"] = { nil },
-		f = {
+		["S"] = { ":SaveSession<cr>", "Save Session" },
+		["c"] = { nil },
+		F = {
 			name = "Focus",
 			t = { ":FocusToggle<cr>", "Toggle" },
 			a = { ":FocusSplitNicely<cr>", "Auto" },
@@ -20,6 +26,12 @@ return {
 		},
 		p = {
 			C = { ":PackerClean<cr>", "Clean" },
+			p = {
+				function()
+					require("telescope").extensions.packer.packer()
+				end,
+				"Packer Search",
+			},
 		},
 		b = {
 			name = "BufferLine",
@@ -27,19 +39,28 @@ return {
 			p = { ":BufferLineTogglePin<cr>", "Pin" },
 			c = { ":BufferLinePickClose<cr>", "Close" },
 		},
-		s = {
-			f = { "<cmd>Telescope find_files<CR>", "Files" },
-			o = { "<cmd>Telescope oldfiles<CR>", "Old Files" },
-			w = { "<cmd>Telescope live_grep<CR>", "Words" },
-			b = { "<cmd>Telescope buffers<CR>", "Buffers" },
-			c = { "<cmd>Telescope commands<CR>", "Commands" },
-			k = { "<cmd>Telescope keymaps<CR>", "Keys" },
-			h = { "<cmd>Telescope help_tags<CR>", "Help" },
-			n = { "<cmd>Telescope notify<CR>", "Notification" },
-			M = { "<cmd>Telescope man_pages<CR>", "Man" },
-			r = { "<cmd>Telescope registers<CR>", "Registers" },
-			s = { "<cmd>Telescope<cr>", "Telescope" },
-			m = { "<cmd>Telescope marks<CR>", "Marks" },
+		f = {
+			name = "Files",
+			h = { nil },
+			s = { ":SearchSession<cr>", "Sessions" },
+			l = {
+				function()
+					require("telescope").extensions.luasnip.luasnip({})
+				end,
+				"Luasnip",
+			},
+			z = {
+				function()
+					require("telescope").extensions.zoxide.list()
+				end,
+				"Zoxide",
+			},
+			P = {
+				function()
+					require("telescope").extensions.project.project({})
+				end,
+				"Project",
+			},
 			d = {
 				name = "Dap",
 				c = {
@@ -73,46 +94,103 @@ return {
 					"Frames",
 				},
 			},
-			p = {
+		},
+		s = {
+			b = { nil },
+			r = {
 				function()
-					require("telescope").extensions.packer.packer()
+					require("telescope.builtin").registers()
 				end,
-				"Packer",
+				"Registers",
 			},
-			l = {
+			t = { "<cmd>Telescope<cr>", "Telescope" },
+			m = {
 				function()
-					require("telescope").extensions.luasnip.luasnip({})
+					require("telescope.builtin").man_pages()
 				end,
-				"Luasnip",
-			},
-			P = {
-				function()
-					require("telescope").extensions.project.project({})
-				end,
-				"Project",
+				"Man",
 			},
 		},
 		d = {
 			name = "Debugger",
-			b = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Toggle Breakpoint" },
-			B = { "<cmd>lua require'dap'.clear_breakpoints()<cr>", "Clear Breakpoints" },
-			c = { "<cmd>lua require'dap'.continue()<cr>", "Continue" },
-			i = { "<cmd>lua require'dap'.step_into()<cr>", "Step Into" },
-			l = { "<cmd>lua require'dapui'.float_element('breakpoints')<cr>", "List Breakpoints" },
-			o = { "<cmd>lua require'dap'.step_over()<cr>", "Step Over" },
-			q = { "<cmd>lua require'dap'.close()<cr>", "Close Session" },
-			Q = { "<cmd>lua require'dap'.terminate()<cr>", "Terminate" },
-			r = { "<cmd>lua require'dap'.repl.toggle()<cr>", "REPL" },
-			s = { "<cmd>lua require'dapui'.float_element('scopes')<cr>", "Scopes" },
-			t = { "<cmd>lua require'dapui'.float_element('stacks')<cr>", "Threads" },
-			u = { "<cmd>lua require'dapui'.toggle()<cr>", "Toggle Debugger UI" },
-			w = { "<cmd>lua require'dapui'.float_element('watches')<cr>", "Watches" },
-			x = { "<cmd>lua require'dap.ui.widgets'.hover()<cr>", "Inspect" },
-		},
-		S = {
-			name = "Session",
-			s = { ":SaveSession<cr>", "Save" },
-			r = { ":SearchSession<cr>", "Reload" },
+			b = {
+				function()
+					require("dap").toggle_breakpoint()
+				end,
+				"Toggle Breakpoint",
+			},
+			B = {
+				function()
+					require("dap").clear_breakpoints()
+				end,
+				"Clear Breakpoints",
+			},
+			c = {
+				function()
+					require("dap").continue()
+				end,
+				"Continue",
+			},
+			i = {
+				function()
+					require("dap").step_into()
+				end,
+				"Step Into",
+			},
+			o = {
+				function()
+					require("dap").step_over()
+				end,
+				"Step Over",
+			},
+			q = {
+				function()
+					require("dap").close()
+				end,
+				"Close Session",
+			},
+			Q = {
+				function()
+					require("dap").terminate()
+				end,
+				"Terminate",
+			},
+			r = {
+				function()
+					require("dap").repl.toggle()
+				end,
+				"REPL",
+			},
+			s = {
+				function()
+					require("dapui").float_element("scopes")
+				end,
+				"Scopes",
+			},
+			t = {
+				function()
+					require("dapui").float_element("stacks")
+				end,
+				"Threads",
+			},
+			u = {
+				function()
+					require("dapui").toggle()
+				end,
+				"Toggle Debugger UI",
+			},
+			w = {
+				function()
+					require("dapui").float_element("watches")
+				end,
+				"Watches",
+			},
+			x = {
+				function()
+					require("dap.ui.widgets").hover()
+				end,
+				"Inspect",
+			},
 		},
 	},
 }
