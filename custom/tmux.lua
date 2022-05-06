39bc4e75699b8c47fb03db94b387a44a75a2f40d
Pham Dat <phamthanhdat17092004@gmail.com>
2022-05-06 13:13:11 +0700

    fsadf

--- custom/tmux.lua
+++ custom/tmux.lua
@@ -1,16 +1,17 @@
-local has_tmux_plugin, _ = pcall(require, "tmux")
-
-if vim.fn.exists "$TMUX" == 1 and has_tmux_plugin then
-  require("tmux").setup {
-    copy_sync = {
-      enable = false,
-    },
-    navigation = {
-      cycle_navigation = true,
-      enable_default_keybindings = true,
-    },
-    resize = {
-      enable_default_keybindings = true,
-    },
-  }
+if vim.fn.exists "$TMUX" == 1 then
+  local has_tmux_plugin, _ = pcall(require, "tmux")
+  if has_tmux_plugin then
+    require("tmux").setup {
+      copy_sync = {
+        enable = false,
+      },
+      navigation = {
+        cycle_navigation = true,
+        enable_default_keybindings = true,
+      },
+      resize = {
+        enable_default_keybindings = true,
+      },
+    }
+  end
 end
