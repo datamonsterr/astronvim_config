17d4d34446e03554f483308b7d0485d402f41040
Pham Dat <phamthanhdat17092004@gmail.com>
2022-05-30 17:58:48 +0700

    fix: python mappings

--- custom/lang_specify.lua
+++ custom/lang_specify.lua
@@ -7,7 +7,7 @@ local map = function(lang, mode, lhs, rhs, opts)
   })
 end
 
-map("run_python", "py", "n", "<F5>", ":!python %<cr>", { desc = "Run python code" })
+map("py", "n", "<F5>", ":!python %<cr>", { desc = "Run python code" })
 
 map("cpp", "n", "<F4>", ":cd %:h<cr> :!g++ % -g -o %:r.out<cr>", { desc = "Compile cpp to out file to debug" })
 map("cpp", "n", "<F5>", ":cd %:h<cr> :!g++ % && ./a.out<cr> :!rm a.out <cr>", { desc = "Run cpp code directly" })
