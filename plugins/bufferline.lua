28fa8d7f45ddab2424d1f3a8db4fb93f469d4414
Pham Dat <phamthanhdat17092004@gmail.com>
2022-06-03 11:03:05 +0700

    update small stuff

--- plugins/bufferline.lua
+++ plugins/bufferline.lua
@@ -3,7 +3,7 @@ return {
     offsets = {
       { filetype = "neo-tree", text = "File Explorer", highlight = "NeoTreeNormal", padding = 0 },
     },
-    show_close_icons = false,
+    show_close_icon = false,
     show_buffer_close_icons = false,
     themable = true,
   },
