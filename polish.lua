27056e9ad554abaa4d86737580132aec860e6b18
Pham Dat <phamthanhdat17092004@gmail.com>
2022-06-12 11:58:05 +0700

    notify about my homework lmao

--- polish.lua
+++ polish.lua
@@ -5,10 +5,6 @@ return function()
   require "user.custom.ui"
   require "user.custom.lang_specify"
 
-  vim.schedule(function()
-    vim.notify("Do you have any homework left?", "info", { title = "Ask yourself" })
-  end)
-
   -- FileType
   vim.filetype.add {
     extension = {
