05221cdd7b87df30b3597341592a2e1aa076f1b5
datamonster <phamdat17092004@gmail.com>
2022-06-14 13:45:56 +0700

    update catppuccin theme

--- plugins/init.lua
+++ plugins/init.lua
@@ -130,5 +130,64 @@ return {
     requires = "nvim-neorg/neorg-telescope",
   },
   ["LunarVim/darkplus.nvim"] = {},
-  ["catppuccin/nvim"] = { as = "catppuccin" },
+  ["catppuccin/nvim"] = {
+    as = "catppuccin",
+    config = function()
+      vim.g.catppuccin_flavour = "frappe"
+      require("catppuccin").setup {
+        integrations = {
+          treesitter = true,
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = "italic",
+              hints = "italic",
+              warnings = "italic",
+              information = "italic",
+            },
+            underlines = {
+              errors = "underline",
+              hints = "underline",
+              warnings = "underline",
+              information = "underline",
+            },
+          },
+          lsp_trouble = false,
+          cmp = true,
+          lsp_saga = false,
+          gitgutter = false,
+          gitsigns = true,
+          telescope = true,
+          nvimtree = {
+            enabled = false,
+            show_root = false,
+            transparent_panel = false,
+          },
+          neotree = {
+            enabled = true,
+            show_root = true,
+            transparent_panel = false,
+          },
+          which_key = false,
+          indent_blankline = {
+            enabled = true,
+            colored_indent_levels = true,
+          },
+          dashboard = false,
+          neogit = false,
+          vim_sneak = false,
+          fern = false,
+          barbar = false,
+          bufferline = true,
+          markdown = true,
+          lightspeed = false,
+          ts_rainbow = true,
+          hop = true,
+          notify = true,
+          telekasten = false,
+          symbols_outline = false,
+        },
+      }
+    end,
+  },
 }
