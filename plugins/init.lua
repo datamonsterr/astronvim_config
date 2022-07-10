28a52da14b61328f8b2a9756c1322cc93034b277
datamonster <phamdat17092004@gmail.com>
2022-07-10 09:10:55 +0700

    update

--- plugins/init.lua
+++ plugins/init.lua
@@ -68,62 +68,8 @@ return {
   ["catppuccin/nvim"] = {
     as = "catppuccin",
     config = function()
-      vim.g.catppuccin_flavour = "mocha"
-      require("catppuccin").setup {
-        term_colors = true,
-        integrations = {
-          treesitter = true,
-          native_lsp = {
-            enabled = true,
-            virtual_text = {
-              errors = "italic",
-              hints = "italic",
-              warnings = "italic",
-              information = "italic",
-            },
-            underlines = {
-              errors = "undercurl",
-              hints = "undercurl",
-              warnings = "undercurl",
-              information = "undercurl",
-            },
-          },
-          lsp_trouble = false,
-          cmp = true,
-          lsp_saga = false,
-          gitgutter = false,
-          gitsigns = true,
-          telescope = true,
-          nvimtree = {
-            enabled = false,
-            show_root = false,
-            transparent_panel = false,
-          },
-          neotree = {
-            enabled = true,
-            show_root = true,
-            transparent_panel = false,
-          },
-          which_key = true,
-          indent_blankline = {
-            enabled = true,
-            colored_indent_levels = true,
-          },
-          dashboard = false,
-          neogit = false,
-          vim_sneak = false,
-          fern = false,
-          barbar = false,
-          bufferline = true,
-          markdown = true,
-          lightspeed = false,
-          ts_rainbow = true,
-          hop = true,
-          notify = true,
-          telekasten = false,
-          symbols_outline = false,
-        },
-      }
+      require "plugins.catppuccin"
     end,
   },
+  ["mickael-menu/zk-nvim"] = {},
 }
